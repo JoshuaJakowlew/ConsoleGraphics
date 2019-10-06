f412836a6c5d5be8f6e39ce6e1b00a3a68dab0b2
Jakowlew <darksmolensksky@gmail.com>
2019-10-07 00:16:26 +0300

    Simplify code Improve perfomance

--- ConsoleGraphics/Texture.cpp
+++ ConsoleGraphics/Texture.cpp
@@ -2,41 +2,35 @@
 
 namespace cg
 {
-	void Texture::setPalette(const Palette& palette)
-	{
-		m_palette = convertToInternalPalette(palette);
-	}
-	
 	bool Texture::loadFromBitmap(std::string_view path)
 	{
-		bitmap_image bitmap(path.data());
+		bitmap_image bitmap{ path.data() };
 		
 		[[unlikely]]
 		if (!bitmap.data())
 			return false;
 		
 		m_size = { bitmap.width(), bitmap.height() };
-
-		std::vector<CHAR_INFO> local_data;
-		if (bool success = convertBitmapToPalette(bitmap, local_data);
-			success)
-		{
-			m_data = std::move(local_data);
-		}
+		m_data = convertBitmapToPalette(bitmap);
+		
+		return true;
 	}
 
-	std::array<rgb_t, 16> Texture::convertToInternalPalette(Palette external)
+	Texture::InternalPalette Texture::convertToInternalPalette(const Palette& external) noexcept
 	{
-		auto internal = std::array<rgb_t, 16>{};
-		for (int i = 0; i < 16; ++i)
-		{
-			internal[i] = rgb_t{ GetRValue(external[i]), GetGValue(external[i]), GetBValue(external[i]) };
-		}
+		auto internal = InternalPalette{};
+
+		std::transform(std::begin(external), std::end(external), std::begin(internal), [](auto e) {
+			return rgb_t{ GetRValue(e), GetGValue(e), GetBValue(e)};
+		});
+
 		return internal;
 	}
 
-	bool Texture::convertBitmapToPalette(const bitmap_image& bitmap, std::vector<CHAR_INFO>& converted_data)
+	std::vector<CHAR_INFO> Texture::convertBitmapToPalette(const bitmap_image& bitmap)
 	{
+		std::vector<CHAR_INFO> converted_data;
+
 		if(converted_data.capacity() < bitmap.pixel_count())
 			converted_data.reserve(bitmap.pixel_count());
 
@@ -44,28 +38,20 @@ namespace cg
 		{
 			for (size_t x = 0; x < bitmap.width(); ++x)
 			{
-				if (auto[index, success] = convertColorToPalette(bitmap.get_pixel(x, y));
-					success)
-				{
-					converted_data.emplace_back(makeCharInfo(L' ', index, 0));
-				}
-				else return false;
+				auto index = convertColorToPalette(bitmap.get_pixel(x, y));
+				converted_data.emplace_back(makeCharInfo(L' ', index, 0));
 			}
 		}
 
-		return true;
+		return converted_data;
 	}
 
-	std::tuple<size_t, bool> Texture::convertColorToPalette(rgb_t color)
+	size_t Texture::convertColorToPalette(rgb_t color) const noexcept
 	{
 		color = find_nearest_color(color, m_palette.begin(), m_palette.end());
-		if (auto it = std::find(m_palette.begin(), m_palette.end(), color);
-			it != m_palette.end())
-		{
-			return { it - m_palette.begin(), true };
-		}
 
-		return { 0, false };
+		auto it = std::find(m_palette.begin(), m_palette.end(), color);
+		return it - m_palette.begin();
 	}
 
 } // namespace cg
